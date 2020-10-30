c87f83fcd47148ad283e5c27ecf0a9d312cd7f81
Juan Fernando <=>
2020-10-30 18:43:22 -0500

    Se agrega implementacion para por medio del metodo doOnNext imprimir un observable reactivo de flux cuando llegue información y suscribirnos a el para poder ejecutarlo

--- src/main/java/com/reactivo/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactivo/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,24 @@
 package com.reactivo.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Juan", "Sonia", "Sofi", "Pablo")
+				.doOnNext(System.out::println);
+
+		nombres.subscribe();
 	}
 
 }
