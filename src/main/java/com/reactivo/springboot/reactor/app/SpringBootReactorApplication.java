8061b1b6600da39f3398819981d26e62c0361998
Juan Fernando <=>
2020-10-30 18:52:07 -0500

    Se controlan errores como segundo argumento de la subscripción

--- src/main/java/com/reactivo/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/reactivo/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.reactivo.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,8 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger LOG = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
@@ -15,10 +19,19 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Juan", "Sonia", "Sofi", "Pablo")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Juan", "Sonia", "", "Pablo")
+				.doOnNext(e -> {
+					if (e.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden estar vacíos");
+					} else {
+						System.out.println();
+					}
+				});
 
-		nombres.subscribe();
+		nombres.subscribe(
+				LOG::info,
+				error -> LOG.error(error.getMessage())
+		);
 	}
 
 }
